2873a04877dec2ef44360fc2dc9f5e21b635d02c
Diana <m.diana.dev@gmail.com>
2024-09-29 15:40:54 +0300

    added headers in instance (fixed 403 error)

--- src/common/api/instance.ts
+++ src/common/api/instance.ts
@@ -3,4 +3,7 @@ import axios from "axios"
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1",
   withCredentials: true,
+  headers: {
+    "API-KEY": "7846650e-e24d-4f57-91de-bd81d9965c92",
+  },
 })
